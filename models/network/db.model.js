08283e21c3ba20e10116747ff73b92c642e93bb0
Daniel Harrison <daniel.equiknox@gmail.com>
2020-02-05 22:24:18 +1000

    Aristocratic nuances

--- models/network/db.model.js
+++ models/network/db.model.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose')
 const schema = require('./db.schema')
 const model = mongoose.model('project', schema)
 
+
 model.networkList = async (args, owner) => {
+	
+	await new Promise(r => setTimeout(r, 1500));
+
 	return [
 		{
 			_id: '111',
-			name: 'Ecommerce Network | Prod',
+			name: 'Ecommerce Network - Prod',
 			nodes: [
 				{
 					_id: '111-1',
@@ -34,20 +38,20 @@ model.networkList = async (args, owner) => {
 					name: '111-4',
 					ip: '111-1',
 					provider: 'DigitalOcean',
-					status: 'OFFLINE'
+					status: 'ONLINE'
 				},
 				{
 					_id: '111-5',
 					name: '111-5',
 					ip: '111-2',
 					provider: 'AWS',
-					status: 'PENDING'
+					status: 'ONLINE'
 				}
 			]
 		},
 		{
 			_id: '222',
-			name: 'Ecommerce Network | Staging',
+			name: 'Ecommerce Network - Staging',
 			nodes: [
 				{
 					_id: '222-1',
@@ -62,6 +66,47 @@ model.networkList = async (args, owner) => {
 					ip: '222-2',
 					provider: 'GCP',
 					status: 'OFFLINE'
+				}
+			]
+		},
+		{
+			_id: '333',
+			name: 'Ecommerce Network - Dev',
+			nodes: [
+				{
+					_id: '333-1',
+					name: '333-1',
+					ip: '333-1',
+					provider: 'AWS',
+					status: 'ONLINE'
+				},
+				{
+					_id: '333-2',
+					name: '333-2',
+					ip: '333-2',
+					provider: 'GCP',
+					status: 'PENDING'
+				},
+				{
+					_id: '333-2',
+					name: '333-2',
+					ip: '333-2',
+					provider: 'GCP',
+					status: 'PENDING'
+				},
+				{
+					_id: '333-2',
+					name: '333-2',
+					ip: '333-2',
+					provider: 'GCP',
+					status: 'PENDING'
+				},
+				{
+					_id: '333-2',
+					name: '333-2',
+					ip: '333-2',
+					provider: 'GCP',
+					status: 'PENDING'
 				}
 			]
 		}
