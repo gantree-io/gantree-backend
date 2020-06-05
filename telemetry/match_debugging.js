c3d6013d0f0ef2216e4737b710602fb07e696460
Denver <23412100+DrTexx@users.noreply.github.com>
2020-06-05 14:37:22 +1000

    regex replace sample working

--- telemetry/match_debugging.js
+++ telemetry/match_debugging.js
@@ -9,6 +9,9 @@ const metric_list = [
 // const real_query = "rate(Bill_had_a_hat[5m])"
 // const real_query = "rate(Bill_had[5m])"
 const real_query = "rate(Bill_had[5m]) + rate(Bill[5m])"
+// const real_query = "rate(Bill_had_a_hat[5m])"
+
+let valid_matches = []
 
 for (metric of metric_list) {
 
@@ -27,11 +30,17 @@ for (metric of metric_list) {
     if (regex_matches) {
         console.log("    Regex match/es found! ---- " + colors.BgGreen + colors.FgBlack + JSON.stringify(regex_matches) + colors.Reset)
 
-        for (regex_match of regex_matches) {
+        for (const [regex_match_index, regex_match] of regex_matches.entries()) {
             console.log("    Checking match '" + colors.BgGreen + colors.FgBlack + regex_match + colors.Reset + "' is exactly '" + colors.BgBlue + metric + colors.Reset + "'...")
 
             if (regex_match === metric) { // probably can't be just the first in practice
                 console.log(colors.FgGreen + "        ACTUAL REAL MATCH FOUND! YAY!" + colors.Reset)
+                valid_matches.push({
+                    regex_obj: name_regex,
+                    regex_string: regex_string,
+                    match: regex_match,
+                    match_index: regex_match_index
+                })
             } else {
                 console.log(colors.FgRed + "        match is not exact" + colors.Reset)
             }
@@ -44,5 +53,48 @@ for (metric of metric_list) {
 
 }
 
+console.log("----VALID MATCHES---- ")
+for (valid_match of valid_matches) {
+    console.log(JSON.stringify(valid_match))
+}
+console.log(" ")
+
+let modified_query = real_query
+
+for (valid_match of valid_matches) {
+    console.log(`----[${valid_match.match_index}]`)
+    console.log("valid match: " + JSON.stringify(valid_match))
+    console.log("expected match: " + valid_match.match)
+
+    const valid_regexp = valid_match.regex_obj
+    modified_query = modified_query.replace(valid_regexp, (match_candidate) => {
+        // if (metric_list.includes(match_candidate)) { //gives false positives
+        console.log("--")
+        console.log("candidate: " + match_candidate)
+        if (match_candidate === valid_match.match) {
+            console.log("real match: " + match_candidate)
+            const changed = `${match_candidate}{job="some_name"}`
+            console.log("replaced with: " + changed)
+            return changed
+        } else {
+            console.log("bad match: " + match_candidate)
+            return match_candidate
+        }
+        // console.log("match candidate: " + match_candidate)
+        // console.log("match we got earlier: " + valid_match.match)
+        // console.log(match_candidate + " === " + valid_match.match)
+        // if (match_candidate === valid_match.match) {
+        //     return `${match_candidate}{job="some_name"}`
+        // } else {
+        //     return match_candidate
+        // }
+    })
+    console.log("replace for " + valid_match.match + ": " + modified_query)
+}
+
+// use this regex, run a function for each match you find
+// function
+// for each regex match, check it is an exact metric name
 
 
+console.log(modified_query)
