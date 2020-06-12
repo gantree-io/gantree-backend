430884cac322f3874d5e726b99d7f12d31871def
Denver <23412100+DrTexx@users.noreply.github.com>
2020-06-12 16:34:29 +1000

    automatic metric scoping for query_range queries

--- telemetry/promrouter.js
+++ telemetry/promrouter.js
@@ -1,6 +1,8 @@
 const got = require('got')
-const promIp = 'http://64.225.2.22' // TODO: FIXME: do not leave this static
-const promPort = 9090
+const { scope_metrics } = require('./query_modifier')
+
+const PROM_IP = 'http://64.225.2.22' // TODO: FIXME: do not leave this static
+const PROM_PORT = 9090
 const apiWhitelist = [
     "/api/v1/query_range",
     // "/api/v1/metadata",
@@ -8,30 +10,18 @@ const apiWhitelist = [
     // "/api/v1/series"
 ]
 
-// This is terrible, terrible code, but I'm totally out of my depth here
-const addJobScope = (req, job) => {
-    // if the query contains an opening curly
-    if (req.query.query.includes("{")) {
-        // split after first {
-        // 'polkadot_block_height{instance="157.245.8.4:80",}'
-        let separate_filters = req.query.query.split("{")
-        console.log("filters separated:" + separate_filters)
-        // [ 'polkadot_block_height' , instance="157.245.8.4:80",}
-
-        let filters = separate_filters[1].split(",")
-        console.log("filters:" + filters)
-    }
-    // no opening curly
-    else {
-        // add {job="JOB_NAME"} to query
-        req.query.query = `${req.query.query}{job="${job}"}` // w/o double quotes you're given everything
-    }
-    console.log("modified query:" + req.query.query)
-    return req
+function log_promql_query(req) {
+    if (!req) { console.log("req arg missing") }
+    if (req.query && req.query.query) { console.log(`\n--PromQL Query: ${req.query.query}\n`) }
+    else { console.log("no req.query.query found") }
 }
 
-const queryModifiers = {
-    "/api/v1/query_range": addJobScope
+async function get_metric_names(req) {
+    const metadata = await got(`${PROM_IP}:${PROM_PORT}/api/v1/metadata`, { method: 'GET', searchParams: { ...req.query } })
+    const raw_metric_list = JSON.parse(metadata.body)
+    let metric_names = []
+    for (key in raw_metric_list.data) { metric_names.push(key) }
+    return metric_names
 }
 
 /**
@@ -43,18 +33,31 @@ const handleReq = async (req, res) => {
     // TODO: is below handled with any weird JS-ness? Is this secure?
     console.log("params:" + JSON.stringify(req.params))
     console.log("original url:" + req.originalUrl)
+
     // unique network name to scope to in query
-    const job = "team1network1"
-    // modify requests originalUrl such that the "query" query has a job scope added to the end of it (i.e. query{job=jobname})
-    // req.originalUrl = req.originalUrl.replace(req.query.query, `${req.query.query}%7Bjob%3D"${job}"%7D`)
+    const job_name = "team1network1"
+
+    // Get list of metric names
+    const metric_names = await get_metric_names(req)
+
+    // log the promql query if it exists
+    log_promql_query(req)
+
+    // ---NO WHITELIST BELOW, COMMENT OUT UNLESS TESTING
+    // const result = await got(`${PROM_IP}:${PROM_PORT}${req.originalUrl}`, { method: 'GET', searchParams: { ...req.query } })
+    // // console.log(result)
+    // console.log(result.requestUrl)
+    // return result.body
+    // ---NO WHITELIST ABOVE, COMMENT OUT UNLESS TESTING
 
     if (apiWhitelist.includes(req.params[0])) {
         switch (req.params[0]) {
             case "/api/v1/query_range":
-                req = addJobScope(req, "team1network1")
+                req.query.query = scope_metrics(req.query.query, metric_names, job_name)
+                console.log("scoped query: " + req.query.query)
                 break
         }
-        const result = await got(`${promIp}:${promPort}${req.originalUrl}`, { method: 'GET', searchParams: { ...req.query } })
+        const result = await got(`${PROM_IP}:${PROM_PORT}${req.originalUrl}`, { method: 'GET', searchParams: { ...req.query } })
         // console.log(result)
         console.log(result.requestUrl)
         return result.body
