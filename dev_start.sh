57fb3a6560de15c6117aa7550b089d2333b676dd
Denver <23412100+DrTexx@users.noreply.github.com>
2020-06-30 15:37:33 +1000

    update dev_start to use process.json (fixes watch)

--- dev_start.sh
+++ dev_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pushd ../telemetry-action-server/bin/grafana-6.7.3/bin && pm2 start grafana-server && popd
-pm2 start ./server.js --name "gantree-server" --watch
+# pushd ../telemetry-action-server/bin/grafana-6.7.3/bin && pm2 start grafana-server && popd
+pm2 start ./process.json
 echo "run this too:"
 echo "--> sudo systemctl start mongod"
